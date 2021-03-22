400d1964517a22eb244fa716d143b15a30365ff2
Donovan McGuire <donovanjmcguire@gmail.com>
2021-03-22 16:44:11 -0500

    Update customerSelect.js

--- forms/customerSelect/customerSelect.js
+++ forms/customerSelect/customerSelect.js
@@ -1,21 +1,27 @@
 let req = ""
 let query = ""
-let results = ""
+let results = []
 let pw = "Bolognabaddie37!"  
 let netID = "djm42254"
 let allCustomers = []
 
 
-btnName.onclick=function(){
-  query = "SELECT `name` FROM customer"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=djm42254&query=" + query)
-  if (req.status == 200) {
-  results = JSON.parse(req.responseText)
-  let message = ""
-  for (i = 0; i < results.length; i++)
-      message = message + results[i]
-  txt1.value = message
- }else { 
-        txt1.value = "Error code: " + req.status
-}
+
+txtaCustomers.onclick=function(){
+   query = "SELECT * FROM customer"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=djm42254&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        if (results.length == 0)    
+           lblMessage.textContent = "There are no pets in the database."
+        else {        
+           let message = ""
+           for (i = 0; i < results.length; i++)
+               message = message + results[i][1] + "\n"
+           txtaCustomers.value = message
+        } // end else
+
+    } else   // the transit didn't work - bad wifi? server turned off?
+        lblMessage1.textContent = "Error code: " + req.status
 }
